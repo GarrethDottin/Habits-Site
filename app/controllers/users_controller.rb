14450d02dc3fd565985ea09290e0565c590e3881
gd7627a <garreth.dottin@gmail.com>
2013-12-25 22:58:45 -0800

    rendered information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,17 @@ class UsersController < ApplicationController
 
   def index
     @habit = User.new
+    @first_Story = User.find(1)
+    @second_Story = User.find(2)
+    @third_Story = User.find(3)
+    @fourth_Story = User.find(4)
   end
 
   def create
     @user = User.new(params[:user])
-    @user.save
+    if @user.save
+      render
+    end
     redirect_to :back
   end
 
--- db/migrate/20131223120921_create_user_stories.rb
+++ db/migrate/20131223120921_create_user_stories.rb
@@ -3,8 +3,6 @@ class CreateUserStories < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.text :habit
-      t.binary :profile
-
     end
   end
 end
